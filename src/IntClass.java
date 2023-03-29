f9d2decfbd3eaab9dcb2d123d4b1d0e1cb78e9a8
Dmytro Bukin <aftosem@ukr.net>
2023-03-30 00:19:13 +0300

    my dz-4 commit

--- src/IntClass.java
+++ src/IntClass.java
@@ -2,7 +2,7 @@ public class IntClass {
     private int valueA = 7;
 
     public int printA() {
-        System.out.println(this.valueA);
+        System.out.println("---<" + this.valueA + ">---");
         return this.valueA;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         IntClass intClass = new IntClass();
-        System.out.printf("Value A = %d", intClass.printA());
+        System.out.printf("~~~<Value A = %d>~~~", intClass.printA());
     }
 }
